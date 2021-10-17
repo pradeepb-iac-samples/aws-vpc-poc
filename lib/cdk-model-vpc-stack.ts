9180a73f3d85ea66170ca77a564f74feb194e93a
Pradeep <pbiyagamage@rbauction.com>
2021-10-17 14:34:08 -0700

    Comments added

--- lib/cdk-model-vpc-stack.ts
+++ lib/cdk-model-vpc-stack.ts
@@ -6,11 +6,11 @@ import { ModelNetworking } from './model-networking';
 
 export class CdkModelVpcStack extends cdk.Stack {
 
-  //public readonly modelVPC: ModelNetworking;
-
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    //Following creates a VPC based on properties passed on to 
+    //custom constuct 'ModelNtworking' 
     const modelVPC = new ModelNetworking(this, 'ModelNetworkingConstruct', {
       cidr: '10.0.0.0/16',
       maxAzs: 1,
